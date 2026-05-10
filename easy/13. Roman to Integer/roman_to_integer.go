0391af4c1a220173bf2f9ba3a6d3d181b3ba085f
Darrkkens <fernandohenrique.brandalise@gmail.com>
2026-05-10 14:08:01 -0300

    leet code roman

---
+++ easy/13. Roman to Integer/roman_to_integer.go
@@ -0,0 +1,27 @@
+package roman_to_integer
+
+func romanToInt(s string) int {
+	values := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	total := 0
+
+	for i := 0; i < len(s); i++ {
+		current := values[s[i]]
+
+		if i+1 < len(s) && current < values[s[i+1]] {
+			total -= current
+		} else {
+			total += current
+		}
+	}
+
+	return total
+}
